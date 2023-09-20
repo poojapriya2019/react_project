a87adb2b6e9954109093e658b35bd8c179bc1b72
poojapriya2019 <37402689+poojapriya2019@users.noreply.github.com>
2023-09-20 17:08:13 +0530

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import './App.css'
 function App() {
     return (
      <div>
         <h1>App Component</h1>
-        <p1>Render component</p1>
+        <p1 className='text-xl' >Render component</p1>
      </div>
     )
 }
